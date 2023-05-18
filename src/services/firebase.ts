27f6b534e2e095d5bd7d6ad64c5c6c9e10b7a546
Proveyron <87431358+Proveyron@users.noreply.github.com>
2023-05-18 01:26:12 -0300

    primeira versão
    primeira versão

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCeVEkmyFxpj8MjVFprMCpblq8s5S9wH1k",
+    authDomain: "aps-desenvolvimento-web.firebaseapp.com",
+    projectId: "aps-desenvolvimento-web",
+    storageBucket: "aps-desenvolvimento-web.appspot.com",
+    messagingSenderId: "626961175902",
+    appId: "1:626961175902:web:b8fca0cd9121b9fa28abcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
