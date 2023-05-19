a160e8cea1fdb5d5ac7902f55890e97c6146216d
Proveyron <87431358+Proveyron@users.noreply.github.com>
2023-05-19 18:54:17 -0300

    teste0

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCeVEkmyFxpj8MjVFprMCpblq8s5S9wH1k",
-    authDomain: "aps-desenvolvimento-web.firebaseapp.com",
-    projectId: "aps-desenvolvimento-web",
-    storageBucket: "aps-desenvolvimento-web.appspot.com",
-    messagingSenderId: "626961175902",
-    appId: "1:626961175902:web:b8fca0cd9121b9fa28abcf"
+    apiKey: "AIzaSyCzZspkELItjFMlfywaYoZBA-kk-M3nbnY",
+    authDomain: "aps-unicarioca-5d83e.firebaseapp.com",
+    projectId: "aps-unicarioca-5d83e",
+    storageBucket: "aps-unicarioca-5d83e.appspot.com",
+    messagingSenderId: "971265183208",
+    appId: "1:971265183208:web:4cff8d09aab1b5e2d2a85d"
 };
 
 // Initialize Firebase
